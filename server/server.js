124baacbc5fdbc31bd91929df9fdbd82cc9da098
Jonathan Huang <jhuan015@ucr.edu>
2016-04-06 17:21:12 -0700

    Basic Express Server. Closes #14

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-//test
+var express = require('express');
+var bodyParser = require('body-parser');
+
+app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client'));
+
+// set up routes HERE
+
+module.exports = app;
