36acb0407cacb975f63c3bf5f7b6a1a5c2930452
anatomij <artjom.svjatickis@gmail.com>
2016-04-07 11:55:32 -0700

    Added views/controllers/server side requests closes #1 closes #2

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(__dirname + '/../client'));
 
 app.use(morgan('dev'));
 
+app.post('/api/users/signin', function(req, res) {
+  res.json({token: 'token'});
+});
+
+app.post('/api/users/signup', function(req, res) {
+  res.json({token: 'token'});
+});
+
+app.get('/api/users/signedin', function(req, res) {
+  res.json({token: 'token'});
+});
+
 // set up routes HERE
 
 module.exports = app;
