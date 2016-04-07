29d358199192ee390a29bf8a45146d89747af64a
Jonathan Huang <jhuan015@ucr.edu>
2016-04-06 17:33:33 -0700

    Add Morgan Middleware for logging

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
+
+app.use(morgan('dev'));
 
 // set up routes HERE
 
