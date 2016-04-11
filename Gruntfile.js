0f6f4a02c5cac575693e721c12577830c4339a0d
Mike Khanoyan <mherkhanoyan@gmail.com>
2016-04-11 12:08:20 -0700

    Add jscs linting.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,123 +1,58 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    concat: {
-      client: {
-        src: [],
-
-        dest: "client/dist/client.js"
-      },
-      lib: {
-        src: [],
-
-        dest: "client/dist/lib.js"
-      },
-      css: {
-        src: [],
-        dest: "client/dist/main.css"
-      }
-    },
-
     copy: {
       build: {
         cwd: 'client',
-        src: [ '**' ],
+        src: ['**'],
         dest: 'client/dist',
-        expand: true
+        expand: true,
       },
     },
 
-
-    nodemon: {
-      dev: {
-        script: 'server/server.js'
-      }
-    },
-
-    uglify: {
-      my_target: {
-        files: {
-          "client/dist/main.min.js": ['client/dist/client.js'],
-          "client/dist/lib.min.js": ['client/dist/lib.js']
-        }
-      }
-    },
-
-    jshint: {
-      files: [
-        'client/scripts/**/*.js',
-        'server/*.js'
-      ],
-      options: {
-        force: false,
-        jshintrc: '.jshintrc',
-        ignores: [
-          'client/lib/**/*.js',
-          'client/dist/**/*.js'
-        ]
-      }
-    },
-
-    cssmin: {
-      target: {
-        files: [{
-          expand: true,
-          cwd: 'client/dist',
-          src: ['*.css', '!*.min.css'],
-          dest: 'client/dist',
-          ext: '.min.css'
-        }]
-      }
+    clean: {
+      build: ['client/dist/**'],
+      deploy: [],
     },
 
     shell: {
       bower: {
-        command: 'bower install'
+        command: 'bower install',
       },
       prodServer: {
-        command: 'node server/server.js'
-      }
+        command: 'node server/index.js',
+      },
     },
 
-    watch: {
-      all: {
-        files: [
-
-        ],
-        tasks: [
-          'concat',
-          'uglify',
-          'cssmin'
-        ]
-      }
+    jscs: {
+      src: './',
+      options: {
+        config: '.jscsrc',
+        esnext: true, // If you use ES6 http://jscs.info/overview.html#esnext
+        verbose: true, // If you need output with rule names http://jscs.info/overview.html#verbose
+        fix: false, // Autofix code style violations when possible.
+        requireCurlyBraces: ['if'],
+      },
     },
 
-    clean: {
-      build: [],
-      deploy: []
-    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-shell');
 
-
   grunt.registerTask('build', [
-    'clean:build', 'copy', 'concat', 'uglify', 'cssmin', 'jshint'
+    'clean:build', 'copy'//, 'concat', 'uglify', 'cssmin', <-- add those later
   ]);
 
-  grunt.registerTask('deploy', function(n) {
-    grunt.task.run([ 'shell:bower', 'build', 'clean:deploy']);
+  grunt.registerTask('deploy', function (n) {
+    grunt.task.run(['shell:bower', 'build', 'clean:deploy']);
   });
-
-
 
 };
