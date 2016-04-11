52c20eb5e906b1a3435f044984f8ba8385f6ab11
Jonathan Huang <jhuan015@ucr.edu>
2016-04-11 15:19:06 -0700

    Basic Sockets Connection Working

--- client/app/chat/chat.js
+++ client/app/chat/chat.js
@@ -1,32 +1,46 @@
 angular.module('genesis.chat', [])
 
-.controller('ChatController', function ($scope) {
+.controller('ChatController', function ($scope, Socket) {
+
+  Socket.connect();
+
+  $scope.$on('$locationChangeStart', function(event) {
+    Socket.disconnect(true);
+  })
+
+  // Socket.on('chat message', function () {
+  //   console.log('i received a message');
+  // });
 
   $scope.exist = 'happy';
-  $scope.broadcast = function (msg) {
-    // Socket.socket
-  };
+  // $scope.broadcast = function (msg) {
+  //   Socket.socket
+  // };
 
 
 })
-
-.factory('Socket', function () {
-  var socket = io();
-  $('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-  });
-  socket.on('chat message', function(msg){
-    console.log('tried');
-    $('#messages').append($('<li>').text(msg));
-  });
-
-  // refactor into non JQuery
-  // socket.broad
-
-  return {
-    emit: 'emit',
-    listen: 'on'
-  };
+.factory('Socket', function (socketFactory) {
+  return socketFactory();
 });
+
+
+
+//   var socket = io();
+//   $('form').submit(function(){
+//     socket.emit('chat message', $('#m').val());
+//     $('#m').val('');
+//     return false;
+//   });
+//   socket.on('chat message', function(msg){
+//     console.log('tried');
+//     $('#messages').append($('<li>').text(msg));
+//   });
+
+//   // refactor into non JQuery
+//   // socket.broad
+
+//   return {
+//     emit: 'emit',
+//     listen: 'on'
+//   };
+// });
