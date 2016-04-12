df54c8e9316eafd767304c8060c42811ac0c7adb
Mike Khanoyan <mherkhanoyan@gmail.com>
2016-04-12 11:41:34 -0700

    Start working on api req handlers.

---
+++ server/lib/request-handler.js
@@ -0,0 +1,26 @@
+var Bid = require('./../db/config').Bid;
+var Auction = require('./../db/config').Auction;
+
+module.exports.postBid = function (req, res) {
+  res.status(200).send('ok')
+
+}
+
+module.exports.deleteBid = function (req, res) {
+  res.status(200).send('ok')
+
+}
+
+module.exports.postAuction = function (req, res) {
+  res.status(200).send('ok')
+
+}
+
+module.exports.modifyAuction = function (req, res) {
+  res.status(200).send('ok')
+
+}
+
+module.exports.deleteAuction = function (req, res) {
+  res.status(200).send('ok')
+}
