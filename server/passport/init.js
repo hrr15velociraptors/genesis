f3f551f64f5d74005f1596ed6cb8747f04e3c982
Mike Khanoyan <mherkhanoyan@gmail.com>
2016-04-13 16:26:42 -0700

    Add server side token based auth and jwt strategy to passport.

--- server/passport/init.js
+++ server/passport/init.js
@@ -1,25 +1,25 @@
 var signin = require('./signin');
 var signup = require('./signup');
+var jwt = require('./jwt');
 var User = require('./../db/config').User;
 
 module.exports = function(passport){
 
 	// Passport needs to be able to serialize and deserialize users to support persistent login sessions
-    passport.serializeUser(function(user, done) {
-        console.log('serializing user: ');
-        console.log(user);
-        done(null, user._id);
+  passport.serializeUser(function(user, done) {
+    console.log('serializing user: ');
+    done(null, user._id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        console.log('deserializing user:', user);
+        done(err, user);
     });
+  });
 
-    passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
-            console.log('deserializing user:', user);
-            done(err, user);
-        });
-    });
-
-    // Setting up Passport Strategies for Login and SignUp/Registration
-    signin(passport);
-    signup(passport);
-
+  // Setting up Passport Strategies for Login and SignUp/Registration
+  signin(passport);
+  signup(passport);
+  jwt(passport);
 }
