fc3bb8b55aaab98db95293eac8118e00129d47c4
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-04-14 16:06:01 -0400

    Fixed chat scroll for live auction messages

--- server/routes.js
+++ server/routes.js
@@ -13,6 +13,7 @@ module.exports = function(app, passport) {
     .delete(passport.authenticate('jwt'), handler.deleteBid);
 
   app.route('/api/auction')
+    .get(passport.authenticate('jwt'), handler.getAuction)
     .post(passport.authenticate('jwt'), handler.postAuction)
     .delete(passport.authenticate('jwt'), handler.deleteAuction)
     .put(passport.authenticate('jwt'), handler.modifyAuction);
