61ab5b62bc3bc5996103ddc2e37593636764bbec
anatomij <artjom.svjatickis@gmail.com>
2016-04-14 14:58:36 -0700

    Fix create auction form and createAucController in services.js

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -1,7 +1,22 @@
 angular.module('genesis.profile',  [])
 .controller('ProfileController', function ($scope, User) {
   $scope.user = {};
+
   User.getUserData().then(function (userData) {
     $scope.user = userData;
   });
 })
+.controller('CreateAucController', function($scope, $http) {
+  $scope.auction = {};
+
+  $scope.createAuction = function () {
+      return $http({
+        method: 'POST',
+        url: '/api/users/auctions',
+        data: $scope.auction
+      })
+      .then(function (res) {
+        console.log('Auction saved in DB');
+      });
+    };
+});
